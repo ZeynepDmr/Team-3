7a2c65e65f59fd61b2d065a0e0fd42a36b8d4b3c
EsraBaldiz <esracagan85@gmail.com>
2023-02-23 15:26:33 +0100

    v3

--- src/main/java/Deneme01/Deneme01.java
+++ src/main/java/Deneme01/Deneme01.java
@@ -2,7 +2,8 @@ package Deneme01;
 
 public class Deneme01 {
     public static void main(String[] args) {
-        System.out.println("master nerdesin?");
-        System.out.println("Ozlemin kedisi bulundu");
+        System.out.println("master nerdesin?");//v1
+        System.out.println("Ozlemin kedisi bulundu");//v2
+        System.out.println("branch olusturma");//v3
     }
 }
