fa0e69dbe211b6584f26548d7f18bcb59f386864
EsraBaldiz <esracagan85@gmail.com>
2023-02-23 15:25:15 +0100

    v2

--- src/main/java/Deneme01/Deneme01.java
+++ src/main/java/Deneme01/Deneme01.java
@@ -3,5 +3,6 @@ package Deneme01;
 public class Deneme01 {
     public static void main(String[] args) {
         System.out.println("master nerdesin?");
+        System.out.println("Ozlemin kedisi bulundu");
     }
 }
